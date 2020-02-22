4fa0a3fd2f53a47822f00caa9438d56d7958a08c
tiro-finale <syubarutusiruto@icloud.com>
2020-02-23 07:25:45 +0900

    dealt with [][] and [[]].

--- bf.c
+++ bf.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#define STK_SIZE (32)
+#define ARR_SIZE (64)
+
+// for "[" and "]"
+typedef struct{
+  // pointer to "["
+  char *arr[STK_SIZE];
+  int top;
+} STACK;
+
+void stkinit(STACK*);
+void push(STACK*, char*);
+char *pop(STACK*);
 
 int main(int argc, char **argv){
 
@@ -11,9 +26,33 @@ int main(int argc, char **argv){
 
   char *input = argv[1];
 
-  char arr[32] = {};
+  // array of Brainfuck.
+  char arr[ARR_SIZE] = {};
+  // current cell.
   char *current = arr;
 
+  // A stack for mapping "[" and "]".
+  STACK bracket;
+  // Initialize a stack.
+  stkinit(&bracket);
+  // pairs[i][0] = pointer of "["
+  // pairs[i][1] = pointer of "]".
+  int i = 0;
+  char *pairs[STK_SIZE][2] = {};
+
+  // Make "[" and "]" pairs.
+  for(char *p = input; *p != '\0'; p++){
+    if(*p == '['){
+      push(&bracket, p);
+    }
+    if(*p == ']'){
+      pairs[i][0] = pop(&bracket); // [
+      pairs[i][1] = p;             // ]
+      i++;
+    }
+  }
+
+  // Execute.
   for(char *p = input; *p != '\0'; p++){
 
     switch(*p){
@@ -43,21 +82,76 @@ int main(int argc, char **argv){
 
       case '[':
         if(*current == 0){
-          p = strchr(input, ']');
+          // Find yourself.
+          for(int i = 0; i < STK_SIZE; i++){
+            if(pairs[i][0] == p){
+              // Seek my pair "]"
+              p = pairs[i][1];
+              break;
+            }
+          }
         }
         break;
 
       case ']':
         if(*current != 0){
-          p = strchr(input, '[');
+          // Find yourself.
+          for(int i = 0; i < STK_SIZE; i++){
+            if(pairs[i][1] == p){
+              // Seek my pair "["
+              p = pairs[i][0];
+              break;
+            }
+          }
         }
         break;
 
       default:
         break;
+    }
+  }
 
+  // visualize array.
+  for(int i = 0; i < ARR_SIZE; i++){
+    printf("|%03d", (unsigned char)arr[i]);
+    if((i+1) % 16 == 0){
+      printf("|\n");
     }
   }
   return 0;
+}
+
+
+void stkinit(STACK *stk){
+  for(int i = 0; i < STK_SIZE; i++){
+    stk->arr[i] = NULL;
+  }
+  stk->top = 0;
+  return;
+}
+
+void push(STACK *stk, char *p){
+  if(stk->top <= STK_SIZE){
+    stk->top++;
+    stk->arr[stk->top] = p;
+  }
+  else{
+    fprintf(stderr, "STACK out of range.\n");
+    exit(1);
+  }
+  return;
+}
+
+char *pop(STACK *stk){
+  int tmp;
+  if(stk->top != 0){
+    tmp = stk->top;
+    stk->top--;
+  }
+  else{
+    fprintf(stderr, "STACK is empty.\n");
+    exit(1);
+  }
+  return stk->arr[tmp];
 }
 
